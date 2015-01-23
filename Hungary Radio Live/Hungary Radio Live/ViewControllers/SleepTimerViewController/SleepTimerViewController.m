0e9328efdca9a164341a142994c0e2acd8e49a4f
Phi Nguyen <phi.nguyen@infonam.com>
2015-01-23 18:48:18 +0700

    Updated sleep timer screen

--- Hungary Radio Live/Hungary Radio Live/ViewControllers/SleepTimerViewController/SleepTimerViewController.m
+++ Hungary Radio Live/Hungary Radio Live/ViewControllers/SleepTimerViewController/SleepTimerViewController.m
@@ -9,8 +9,11 @@
 #import "SleepTimerViewController.h"
 
 @interface SleepTimerViewController ()
+@property (strong, nonatomic) IBOutlet UIView *centerView;
 
 - (IBAction)actionClose:(id)sender;
+@property (strong, nonatomic) IBOutlet UIButton *btControl;
+@property (strong, nonatomic) IBOutlet UIButton *btClose;
 @end
 
 @implementation SleepTimerViewController
@@ -18,6 +21,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [[self navigationController] setNavigationBarHidden:YES animated:YES];
+    
+    [self.btClose setTitleColor:_orange_color_ forState:UIControlStateNormal];
+    
+    [self.btControl.layer setCornerRadius:(IS_IPAD?85.f:65.f)];
+    [self.btControl.layer setMasksToBounds:YES];
+    [self.btControl.layer setBorderWidth:1.0f];
+    [self.btControl.layer setBorderColor:[[UIColor whiteColor] CGColor]];
 }
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
